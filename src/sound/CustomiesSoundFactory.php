2dbfd267d87143961fc0038f92b5c7a7cbe69e66
ItIsDev <tranhuybao000@gmail.com>
2025-09-28 09:49:48 +0700

    Update CustomiesSoundFactory.php

--- src/sound/CustomiesSoundFactory.php
+++ src/sound/CustomiesSoundFactory.php
@@ -16,15 +16,15 @@ use pocketmine\math\Vector3;
 class CustomiesSoundFactory {
 
     /**
-     * Creates and spawns a custom particle at the given position in the world.
+     * Creates and spawns a custom sound at the given position in the world.
      * 
-     * @param World $world The world where the particle should be spawned.
-     * @param Vector3 $pos The position in the world where the particle should appear.
-     * @param string $nameParticle The name of the particle to be created.
+     * @param World $world The world where the sound should be spawned.
+     * @param Vector3 $pos The position in the world where the sound should appear.
+     * @param string $nameSound The name of the sound to be created.
      * 
      * @return void
      */
-    public function createParticle(World $world, Vector3 $pos, string $nameParticle, ?array $players = null): void {
+    public function createParticle(World $world, Vector3 $pos, string $nameSound, ?array $players = null): void {
         $world->addSound($pos, new CustomiesSound($nameParticle), $players);
     }
 }
